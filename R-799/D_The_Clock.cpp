a7b6a6551f7cf69a26ea2e7036676843160eb169
Abhijit25Mishra <abhijitmishra20bcs94@iiitkottayam.ac.in>
2022-06-14 21:22:29 +0530

    5

---
+++ R-799/D_The_Clock.cpp
@@ -0,0 +1,68 @@
+// Aur Bhai Dekhne aagaye ;)
+// Author: Abhijit Mishra
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,popcnt,abm,mmx,tune=native")
+#pragma GCC optimize("fast-math")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+#define pi (3.141592653589)
+#define mod 1000000007
+#define pb push_back
+#define is insert
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define min3(a, b, c) min(c, min(a, b))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+#define rfr(n) for (int i = n - 1; i >= 0; i--)
+#define rep1(i, a, b) for (long long i = a; i <= b; i++)
+#define fr(n) for (long long i = 0; i < n; i++)
+#define nesfr(x, y)                   \
+    for (long long i = 0; i < x; i++) \
+        for (long long j = 0; j < y; j++)
+#define rep(i, a, b) for (long long i = a; i < b; i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+typedef long long int ll;
+typedef long double ld;
+typedef vector<ll> vi;
+#define nl cout << "\n"
+const unsigned int M = 1000000007;
+const int N = 2e5 + 5;
+
+int main()
+{
+    fast;
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        ll arr[] = {0, 70, 140, 210, 280, 350, 601, 671, 741, 811, 881, 951, 1202, 1272, 1342, 1412};
+        string s;
+        ll x = 0;
+        cin >> s >> x;
+        ll min = 0;
+        ll ans = 0;
+        min = 600 * ((int)s[0] - 48) + 60 * ((int)s[1] - 48) + 10 * ((int)s[3] - 48) + (int(s[4]) - 48);
+        // cout << (int)s[0] << " " << (int)s[1] << " " << (int)s[3] << " " << (int)s[4];
+        // cout << min;
+        while (min < 1440)
+        {
+            if (count(begin(arr), end(arr), min))
+            {
+                ans++;
+            }
+            cout << min << " ";
+            min += x;
+        }
+        cout << ans;
+        nl;
+        nl;
+        nl;
+    }
+    return 0;
+}
