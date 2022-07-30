fc53e83424eb72f47d6c35562b3e65654de1e333
Abhijit25Mishra <abhijitmishra20bcs94@iiitkottayam.ac.in>
2022-07-31 00:58:48 +0530

    1 2

---
+++ Practise/1100/B_Difference_of_GCDs.cpp
@@ -0,0 +1,104 @@
+// Aur Bhai Dekhne aagaye ;)
+// Author: Abhijit Mishra
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,popcnt,abm,mmx,tune=native")
+#pragma GCC optimize("fast-math")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+#define pi (3.141592653589)
+#define mod 1000000007
+#define pb push_back
+#define is insert
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define min3(a, b, c) min(c, min(a, b))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+#define rfr(n) for (int i = n - 1; i >= 0; i--)
+#define rep1(i, a, b) for (long long i = a; i <= b; i++)
+#define fr(n) for (long long i = 0; i < n; i++)
+#define nesfr(x, y)                   \
+    for (long long i = 0; i < x; i++) \
+        for (long long j = 0; j < y; j++)
+#define rep(i, a, b) for (long long i = a; i < b; i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+typedef long long int ll;
+typedef long double ld;
+typedef vector<ll> vi;
+#define nl cout << "\n"
+const unsigned int M = 1000000007;
+const int N = 2e5 + 5;
+
+int main()
+{
+    fast;
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        bool ok = true;
+        ll n, l, r;
+        cin >> n >> l >> r;
+        ll a[n + 1];
+        rep(i, 1, n + 1)
+        {
+            if (!ok)
+            {
+
+                break;
+            }
+            else
+            {
+                ll x = l / i;
+                ll y = r / i;
+                ll x1 = l % i;
+                ll y1 = r % i;
+
+                if (x != y)
+                {
+                    a[i] = y * i;
+                }
+                else
+                {
+                    if (x1 == 0)
+                    {
+                        a[i] = x * i;
+                        // cout << "lmao1";
+                    }
+                    else if (y1 == 0)
+                    {
+                        a[i] = y * i;
+                        // cout << "lmao2";
+                    }
+                    else
+                    {
+                        ok = false;
+                        // cout << "lmao3";
+                    }
+                }
+            }
+        }
+        if (ok)
+        {
+            cout << "YES";
+            nl;
+            rep(i, 1, n + 1)
+            {
+                cout << a[i] << " ";
+            }
+            nl;
+        }
+        else
+        {
+            cout << "NO";
+            nl;
+        }
+    }
+    return 0;
+}
